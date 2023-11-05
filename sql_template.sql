01d8e9643d24cccc8f57df98ac0d82c63931c1a5
Minho <96557528+xminao@users.noreply.github.com>
2023-11-05 19:56:17 +0800

    [fixed] Delete sql_template.sql

--- sql_template.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [schema_user].[table_name] (
-    [column_name] [data_type] [nullable],
-    -- Add primary key constraint if db_pk is 'Y'
-    [column_name] [data_type] PRIMARY KEY [nullable],
-    -- Add column comments
-    -- column_comment
-);
